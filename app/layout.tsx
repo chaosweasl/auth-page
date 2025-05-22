050b7759d9734c7c4f8a80cd1ebcff87aa34316b
weasl <iacobserban17danieloff@gmail.com>
2025-05-22 07:02:15 +0300

    got rid of messagebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import MessageBar from "./components/client/MessageBar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,7 +27,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <MessageBar />
       </body>
     </html>
   );
